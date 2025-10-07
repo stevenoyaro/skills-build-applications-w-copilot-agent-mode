f12bcb3e4a39d9f09f823ebcbeb57d039045ae41
stevenoyaro <stevenoyaro254@students.tukenya.ac.ke>
2025-10-07 17:52:24 +0000

    Update ALLOWED_HOSTS and urls.py for Codespace and API root endpoint support

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +27,12 @@ SECRET_KEY = 'django-insecure-l+m5z#fm!@m#vu-jlt+n!gvpgq87$+2-n27n9!#^$qae42fr=k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+codespace_name = os.environ.get('CODESPACE_NAME')
+codespace_host = f"{codespace_name}-8000.app.github.dev" if codespace_name else None
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if codespace_host:
+    ALLOWED_HOSTS.append(codespace_host)
+ALLOWED_HOSTS.append('*')
 
 
 # Application definition
